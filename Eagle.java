d09aa1f5b2b2b23af24657657a3d7623f865d860
Arthur Jouanneau <jouanneauarthur@gmail.com>
2019-05-21 15:21:05 +0200

    Creation de l interface Fly

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,53 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void land() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.printf("%s dives into the sky.%n", this.getName());
+        }
+    }
+
+    /**
+     * swim downward
+     * @param meters altitude increase
+     * @return depth
+     */
+    @Override
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude + meters, 325);
+            System.out.printf("%s fly downward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    /**
+     * swin upward
+     * @param meters altitude decrease
+     * @return depth
+     */
+    @Override
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude - meters, 0);
+            System.out.printf("%s fly upward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    /**
+     * get out of water, must be on surface
+     */
+    @Override
+    public void takeOff() {
+        if (this.flying && this.altitude == 0) {
+            System.out.printf("%s gets out of the air.%n", this.getName());
+        } else {
+            System.out.printf("%s is too deep, it can't get out.%n", this.getName());
+        }
     }
 }
--- Nature.java
+++ Nature.java
@@ -13,15 +13,14 @@ public class Nature {
 
         Eagle eyeCherry = new Eagle("Eye Cherry");
         // TODO : décommente le code suivant afin de le tester
-        /*
+
         eyeCherry.takeOff();
-        eyeCherry.ascend(120);
-        eyeCherry.ascend(30);
+        eyeCherry.ascend(1);
+        eyeCherry.ascend(5);
         eyeCherry.glide();
-        eyeCherry.descend(140);
+        eyeCherry.descend(5);
         eyeCherry.land();
         eyeCherry.descend(9);
         eyeCherry.land();
-        */
     }
 }
